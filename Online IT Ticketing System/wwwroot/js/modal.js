3152dcbfe575866b8f4fbd256c4febb23f7ada92
Zquad13 <bichu1306@gmail.com>
2025-01-07 23:20:20 +0530

    final

---
+++ Online IT Ticketing System/wwwroot/js/modal.js
@@ -0,0 +1,9 @@
+﻿// Open the modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
---
+++ Online IT Ticketing System/wwwroot/js/toggle.js
@@ -0,0 +1,17 @@
+﻿// Disable back and forward browser navigation
+(function () {
+    window.history.pushState(null, "", window.location.href);
+    window.onpopstate = function () {
+        window.history.pushState(null, "", window.location.href);
+    };
+})();
+
+function togglePasswordVisibility() {
+    const passwordField = document.getElementById("password");
+    const passwordFieldType = passwordField.getAttribute("type");
+    if (passwordFieldType === "password") {
+        passwordField.setAttribute("type", "text");
+    } else {
+        passwordField.setAttribute("type", "password");
+    }
+}
